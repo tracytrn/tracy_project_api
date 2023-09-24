73d064ceec6aaa352eb5157915bb5d07c06c6fd5
trntkh <hongtran131199@gmail.com>
2023-09-24 23:20:13 +0700

    Implement gem jwt

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    command = Api::V1::Users::AuthenticateCommand.call(params[:email], params[:password])
+    
+    if command.success?
+      render json: command.result
+    else
+      render json: { error: command.errors[:error] }, status: :unauthorized
+    end
+  end
+end
+
